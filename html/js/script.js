42616de7d717179ee764052676c4f05d624f629a
sibin <sibin1616@gmail.com>
2018-11-09 15:46:26 +0530

    day03 javascript

--- html/js/script.js
+++ html/js/script.js
@@ -9,6 +9,9 @@ function read(){
     console.log(adno);
     var age=document.getElementById("Age").value;
     console.log(age);
+    
+    var dist=((document.getElementById("district")).options[(document.getElementById("district")).selectedIndex]).value;
+    console.log(dist);
     if(age>=18){
         alert("eligible");
     }
@@ -17,4 +20,86 @@ function read(){
     }
 
     
+}
+function add(){
+    var a=parseInt(document.getElementById("op1").value);
+    var b=parseInt(document.getElementById("op2").value);
+    var c=a+b;
+   alert(c);
+    
+}
+
+
+function get(){
+    var a=parseInt(document.getElementById("op1").value);
+    var b=parseInt(document.getElementById("op2").value);
+    var o=document.getElementById("op");
+    var op=(o.options[o.selectedIndex]).value;
+    if(op=="+"){
+        var c=a+b;
+        
+    }
+    else if(op=="-"){
+        var c=a-b;
+        
+    }
+    else if(op=="*"){
+        var c=a*b;
+        
+    }
+    else{
+        if(a==0)
+        {
+            var c="not possible";
+        }
+        else{
+        var c=a/b;
+        
+    }
+    }
+document.getElementById("result").innerHTML="<div class='text-light bg-dark'><b>Result is "+c+"</b></div>";
+}
+
+function checkl(){
+    var a=document.getElementById("op1").value;
+    var b=document.getElementById("op2").value;
+    var c=document.getElementById("op3").value;
+    if(a>b){
+        if(a>c){
+     var res=a;
+        }
+        else{
+          var  res=c;
+        }
+    }
+       
+    else if(b>c){
+       var res=b;
+    }
+    else{
+        var res=c;
+    }
+    document.getElementById("result").innerHTML="<div class='text-light bg-dark'><b>Largest is "+res+"</b></div>";
+
+}
+
+function checks(){
+    var a=document.getElementById("op1").value;
+    var b=document.getElementById("op2").value;
+    var c=document.getElementById("op3").value;
+    var res;
+    if(a<b){
+        if(a<c){
+      res=a;
+        }
+        else{
+            res=c;
+        }
+    }
+       
+    else{
+        res=b;
+    }
+    document.getElementById("result").innerHTML="<div class='text-light bg-dark'><b>smallest is "+res+"</b></div>";
+
 }
